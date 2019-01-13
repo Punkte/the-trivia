21908c9c17f82693b3f9bd1a527753bf18222b16
Punkte <paartheepan94@gmail.com>
2019-01-13 13:34:47 +0100

    initial commit

---
+++ src/views/GameOver/GameOverContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Gameover from './GameOver'
+
+import game from '../../helpers/storage'
+
+class GameOverContainer extends Component {
+	constructor() {
+		super()
+		const score = game.getItem( 'score' )
+		this.state = { score }
+	}
+
+	resetGame() {
+		console.clear()
+		game.clearGame()
+	}
+
+	render() {
+		return ( 
+			<Gameover
+				resetGame={ this.resetGame }
+				score={ this.state.score }
+			/>
+		)
+	}
+} 
+export default GameOverContainer
